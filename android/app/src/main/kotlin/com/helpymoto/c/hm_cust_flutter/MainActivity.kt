92063dbf597b6642ff789c1c7d6e84fe2ee2efcb
avnp16 <avnp16@gmail.com>
2023-08-09 14:01:41 +0530

    Ganeshay Namah

---
+++ android/app/src/main/kotlin/com/helpymoto/c/hm_cust_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.helpymoto.c.hm_cust_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hm_cust_flutter/screens/home/home_screen/home_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(create: (context) => null,),
+
+    ],
+    child: GetMaterialApp(
+        theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    useMaterial3: true,
+    ),
+    home: MyHomePage(),
+    )
+    );
+  }
+}
+
+
